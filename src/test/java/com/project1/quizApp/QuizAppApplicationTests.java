27816312b3891cf3fc030bd246432399e4b1fe76
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2024-04-18 01:11:00 -0500

    Initial Commit

---
+++ src/test/java/com/project1/quizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.quizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
